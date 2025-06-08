281d1514267224d7aa18d5c5c4e73df2a7c6dbae
Adel Harrat <a03l.web@gmail.com>
2025-06-08 18:59:45 +0100

    init arrays & slices

--- main.zig
+++ main.zig
@@ -1,12 +1,3 @@
 const print = @import("std").debug.print;
-const user = @import("models/user.zig");
-const User = user.User;
-const MAX_POWER = user.MAX_POWER;
 
-// This code won't compile if main isn't pub
-pub fn main() void {
-    var u = User.init("Goku", 9001);
-    u.name = "Adel";
-    print("{s}'s power is {d} out of {d}\n", .{ u.name, u.power, MAX_POWER });
-    u.diagnose();
-}
+pub fn main() void {}
