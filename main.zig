d8ca081d87cd611c40580908d8f0f0096debf128
Adel Harrat <a03l.web@gmail.com>
2025-06-08 19:27:31 +0100

    creating slices from arrays

--- main.zig
+++ main.zig
@@ -1,3 +1,12 @@
 const print = @import("std").debug.print;
 
-pub fn main() void {}
+pub fn main() void {
+    var a = [_]i32{ 1, 2, 3, 4, 5 };
+    var end: usize = 3;
+    end += 1;
+    const b = a[1..end];
+    // print("a is {any}\n", .{a});
+    print("b is {any}\n", .{b});
+    b[0] = 69;
+    print("b is {any}\n", .{b});
+}
