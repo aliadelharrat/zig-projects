37a9716b63b7afc2a0b256b3600f1e6076b71f14
Adel Harrat <a03l.web@gmail.com>
2025-06-08 20:16:59 +0100

    if

--- main.zig
+++ main.zig
@@ -1,3 +1,11 @@
-const print = @import("std").debug.print;
+const std = @import("std");
 
-pub fn main() void {}
+pub fn main() void {
+    const name = "adel";
+    const isEqual = if (std.ascii.endsWithIgnoreCase(name, "Adel")) true else false;
+    if (isEqual) {
+        std.debug.print("The name is Adel\n", .{});
+    } else {
+        std.debug.print("The name is NOT Adel\n", .{});
+    }
+}
