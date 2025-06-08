00119bf7fa21a3b07283c5d28867121459ee4710
Adel Harrat <a03l.web@gmail.com>
2025-06-08 22:12:57 +0100

    traffic lights

--- main.zig
+++ main.zig
@@ -1,7 +1,17 @@
 const std = @import("std");
 
-pub fn main() void {
-    for (0..10) |el| {
-        std.debug.print("{d}\n", .{el});
+const TrafficLight = enum {
+    green,
+    yellow,
+    red,
+
+    fn beCarefull(self: TrafficLight) bool {
+        return self == .yellow or self == .red;
     }
+};
+
+pub fn main() void {
+    const currentTrafficLight = TrafficLight.red;
+    std.debug.print("current traffic light is : {s}\n", .{@tagName(currentTrafficLight)});
+    std.debug.print("Should I becarefull? {?}", .{currentTrafficLight.beCarefull()});
 }
