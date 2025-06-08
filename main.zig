e653275d2a540cf85fa99fad5740d23a0b6642d1
Adel Harrat <a03l.web@gmail.com>
2025-06-08 20:27:33 +0100

    switch

--- main.zig
+++ main.zig
@@ -1,13 +1,34 @@
 const std = @import("std");
 
 pub fn main() void {
-    const buttonPressed = 3;
-    const snack = switch (buttonPressed) {
-        1 => "chips",
-        2 => "candy",
-        3 => "soda",
-        4 => "water",
-        else => "not defined",
+    const groceries = []const u8{
+        "Bread",
+        "Eggs",
+        "Chicken",
+        "Apples",
+        "Bananas",
+        "Rice",
+        "Pasta",
+        "Tomato sauce",
+        "Lettuce",
+        "Carrots",
+        "Onions",
+        "Cheese",
+        "Yogurt",
+        "Butter",
+        "Olive oil",
+        "Cereal",
+        "Coffee",
+        "Milk",
+        "Sugar",
+        "Toilet paper",
     };
-    std.debug.print("you've choosed {s}", .{snack});
+
+    for (groceries) |grocery| {
+        if (std.ascii.eqlIgnoreCase(grocery, "milk")) {
+            std.debug.print("You've milk!", .{});
+            break;
+        }
+        std.debug.print("You don't have milk!", .{});
+    }
 }
