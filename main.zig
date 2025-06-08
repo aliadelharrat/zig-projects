9c52767d687d9925428052141af8aa791193846f
Adel Harrat <a03l.web@gmail.com>
2025-06-08 17:51:05 +0100

    init

--- main.zig
+++ main.zig
@@ -1,8 +1,7 @@
 const print = @import("std").debug.print;
+const User = @import("models/user.zig").User;
 
 pub fn main() void {
     const user = User{ .power = 9001, .name = "Goku" };
     print("{s}'s power is {d}\n", .{ user.name, user.power });
 }
-
-pub const User = struct { power: u64, name: []const u8 };
---
+++ models/user.zig
@@ -0,0 +1,1 @@
+pub const User = struct { power: u64, name: []const u8 };
