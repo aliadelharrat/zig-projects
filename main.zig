c97b966e5b5aa94633023ad997e3ad7eb8197e8e
Adel Harrat <a03l.web@gmail.com>
2025-06-08 18:21:39 +0100

    cannot assign to const

--- main.zig
+++ main.zig
@@ -5,7 +5,8 @@ const MAX_POWER = user.MAX_POWER;
 
 // This code won't compile if main isn't pub
 pub fn main() void {
-    const u = User.init("Goku", 9001);
+    var u = User.init("Goku", 9001);
+    u.name = "Adel";
     print("{s}'s power is {d} out of {d}\n", .{ u.name, u.power, MAX_POWER });
     u.diagnose();
 }
