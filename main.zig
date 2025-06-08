f538b00192e223fec28c15478e37d6df776dd315
Adel Harrat <a03l.web@gmail.com>
2025-06-08 20:47:02 +0100

    comparing 2 arrays of different len

--- main.zig
+++ main.zig
@@ -1,23 +1,7 @@
 const std = @import("std");
 
 pub fn main() void {
-    const array_one = [_][]const u8{ "a", "b", "c" };
-    const array_two = [_][]const u8{ "a", "b" };
-
-    const isEql = array_eq(array_one, array_two);
-    std.debug.print("does array_one = array_two ? {?}", .{isEql});
-}
-
-fn array_eq(a1: [3][]const u8, a2: [2][]const u8) bool {
-    if (a1.len != a2.len) {
-        return false;
+    for (0..10) |el| {
+        std.debug.print("{d}\n", .{el});
     }
-
-    for (a1, a2) |a1_el, a2_el| {
-        if (!std.ascii.eqlIgnoreCase(a1_el, a2_el)) {
-            return false;
-        }
-    }
-
-    return true;
 }
