db22b789726039ed93da6e832d368eb200ba29db
Adel Harrat <a03l.web@gmail.com>
2025-06-08 18:00:42 +0100

    add comment

--- main.zig
+++ main.zig
@@ -3,6 +3,7 @@ const user = @import("models/user.zig");
 const User = user.User;
 const MAX_POWER = user.MAX_POWER;
 
+// This code won't compile if main isn't pub
 pub fn main() void {
     const u = User{ .power = 9001, .name = "Goku" };
     print("{s}'s power is {d} out of {d}\n", .{ u.name, u.power, MAX_POWER });
